ca3bf329bacc51810b01a138c23a8eac118449e8
Jørgen P. Tjernø <jtjerno@mylookout.com>
2012-04-04 15:42:24 -0700

    Cleanup.

--- models/require_tool_wrapper.rb
+++ models/require_tool_wrapper.rb
@@ -1,5 +1,6 @@
-java_import 'java.net.URLEncoder'
-java_import 'java.net.URLDecoder'
+java_import 'hudson.slaves.NodeSpecific'
+java_import 'hudson.model.EnvironmentSpecific'
+java_import 'jenkins.model.Jenkins'
 
 class RequireToolWrapper < Jenkins::Tasks::BuildWrapper
   display_name "Require tool installations"
@@ -17,10 +18,9 @@ class RequireToolWrapper < Jenkins::Tasks::BuildWrapper
 
   attr_accessor :tools
 
-  def tool_instance(tool_name)
-    id, name = tool_name.split(';', 2)
-    descriptor = Java.jenkins.model.Jenkins.instance.descriptor(id)
-    tool = descriptor.installations.find {|ti| ti.name == name }
+  def tool_instance(tool)
+    descriptor = Jenkins.instance.descriptor(tool['descriptor_id'])
+    descriptor.installations.find { |ti| ti.name == tool['name'] }
   end
 
   # This mimics hudson.cli.InstallToolCommand#install until it's public.
@@ -30,11 +30,11 @@ class RequireToolWrapper < Jenkins::Tasks::BuildWrapper
     return nil unless exec
 
     node = exec.owner.node
-    if tool.java_kind_of?(Java.hudson.slaves.NodeSpecific)
+    if tool.java_kind_of?(NodeSpecific)
       tool = tool.forNode(node, listener)
     end
 
-    if tool.java_kind_of?(Java.hudson.model.EnvironmentSpecific)
+    if tool.java_kind_of?(EnvironmentSpecific)
       tool = tool.forEnvironment(build.environment(listener))
     end
 
@@ -44,7 +44,12 @@ class RequireToolWrapper < Jenkins::Tasks::BuildWrapper
   # Invoked with the form parameters when this extension point
   # is created from a configuration screen.
   def initialize(attrs = {})
-    @tools = attrs.find_all {|attr, _| attr.start_with?('tool_')}.collect {|_, v| v.empty? ? nil : v}.flatten
+    @tools = []
+    attrs.each do |attr, info|
+      next unless attr.start_with?('tool_') and info and not info.empty?
+      descriptor_id, name = info['tool'].split(';', 2)
+      @tools << {'descriptor_id' => descriptor_id, 'name' => name}
+    end
   end
 
   # Perform setup for a build
@@ -54,12 +59,15 @@ class RequireToolWrapper < Jenkins::Tasks::BuildWrapper
   # @param [Jenkins::Launcher] launcher a launcher for the orderly starting/stopping of processes.
   # @param [Jenkins::Model::Listener] listener channel for interacting with build output console
   def setup(build, launcher, listener)
-    tools.each do |tool|
-      next unless tool["tool"]
-      instance = tool_instance(tool["tool"])
-      instance = install_tool(build, listener, instance)
+    tools.each do |tool_description|
+      next unless tool_description["name"] and tool_description["descriptor_id"]
+      tool = tool_instance(tool_description)
+      unless tool
+        build.halt("Could not look up tool: #{tool_description.inspect}")
+      end
 
-      if instance
+      tool = install_tool(build, listener, tool)
+      if tool
         # TODO: Set env vars.
       else
         build.halt("Could not install tool!")
